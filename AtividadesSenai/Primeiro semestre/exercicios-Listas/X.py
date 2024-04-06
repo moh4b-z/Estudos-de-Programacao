36b5880935b84ab57617c50e23853088436f4d6b
moh4b-z <mohammadsalim10000@gmail.com>
2024-04-05 22:34:28 -0300

    terminiei a lista em python

--- AtividadesSenai/Primeiro semestre/exercicios-Listas/X.py
+++ AtividadesSenai/Primeiro semestre/exercicios-Listas/X.py
@@ -1,21 +1,23 @@
 contador = 1
-
 listaA = []
 listaB = []
-pares = []
-impares = []
 
-while contador <= 30:
+# Preencher a listaA com 6 números
+while contador <= 6:
     numero1 = int(input('Colocar um numero para a lista A: '))
     listaA.append(numero1)
     contador += 1
 
-for i, elemento in enumerate(lista_original):
-    if i % 2 == 0:
-        lista_destino1.append(elemento)
-    else:
-        lista_destino2.append(elemento)
+# Iterar sobre os elementos da listaA
+for indice, elemento in enumerate(listaA):
+    if indice < len(listaA) - 1:  # Verifica se não estamos no último elemento
+        if indice % 2 == 0:
+            listaB.append(listaA[indice + 1])
+        else:
+            listaB.append(listaA[indice - 1])
 
-for numeroIP, numeroP in zip(pares, impares):
 
-print(f'Na lista A tem altodo {len(pares)} numeros pares e {len(impares)} de impares')
+listaB.append(listaA[0])
+
+print(f'Lista A: {listaA}')
+print(f'Lista B: {listaB}')
