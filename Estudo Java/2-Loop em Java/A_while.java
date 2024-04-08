3e7f27e63608f67b05d431c5e4280ca4bd536719
moh4b-z <mohammadsalim10000@gmail.com>
2024-04-07 22:59:02 -0300

    aaa

---
+++ Estudo Java/2-Loop em Java/A_while.java
@@ -0,0 +1,16 @@
+
+public class A_while {
+    public static void main(String[] args) {
+        int contador = 15;
+        int finall = 200;
+        while (contador <= finall) {
+
+            double contador2= (Math.pow(contador,2));
+
+            System.out.print(contador2);
+
+            contador++;
+            
+        }
+    }
+}
---
+++ Estudo Java/4-Lists em Java/A_lista_e_for.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class A_lista_e_for {
+    public static void main(String[] args) {
+
+        // Criar uma lista para armazenar números inteiros
+        List<Integer> numeros = new ArrayList<>();
+    }
+    
+}
