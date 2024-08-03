8ef6e972a61c323c7e599e62d011bbc14d948ed7
moh4b-z <mohammadsalim10000@gmail.com>
2024-08-03 01:30:47 -0300

    quick

---
+++ Javascript/Quicksort/Quicksort_numeros.js
@@ -0,0 +1,36 @@
+function ordenar(lista) {
+    let numerosDeElementos = lista.length
+
+    if (numerosDeElementos <= 1) {
+        return (lista)
+    }
+
+    let pivo = lista[0]
+
+    let maiorQuePivo = []
+    let menorQuePivo = []
+
+    let listaOrdenarda = []
+
+    for (){
+        if(intem > pivo){
+            maiorQuePivo.push(intem)
+        }else{
+            menorQuePivo.push(intem)
+        }
+
+    }
+
+    maiorQuePivo = ordenar(maiorQuePivo)
+    menorQuePivo = ordenar(menorQuePivo)
+
+    for (){
+        listaOrdenarda.push()
+    }
+    listaOrdenarda.push(pivo)
+    for (){
+        listaOrdenarda.push()
+    }
+
+    return lista_ordenada
+}
