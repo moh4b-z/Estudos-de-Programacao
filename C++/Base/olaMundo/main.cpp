fc55835afcfab907249be3b529c9cbdf8c2f76eb
moh4b-z <mohammadsalim10000@gmail.com>
2024-10-18 23:13:04 -0300

    Adição de alguns teste com a linguagem C++ e C

---
+++ C++/Base/olaMundo/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    (void)argc;  // Suprime o aviso para 'argc'
+    (void)argv;  // Suprime o aviso para 'argv'
+    cout << "Ola mundo";
+    return 0;
+}
